7ddb4e29912cf90c76abc928e46d9d1f39f85acc
Stephanie Sheldon <S.Camille.Sheldon>
2019-10-10 15:25:33 -0500

    readFile method

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,13 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class MesoInherit extends MesoAbstract {
 	
-
+	private int [] averages;
 	private static final int AVERAGE_ARRAY_SIZE = 3;
 
+	
+	
 	public MesoInherit(MesoStation mesoStation) {
 		this.station = mesoStation;
 	}
+	
 
+	
+	
+	public ArrayList<MesoStation> readFile(String fileName) throws IOException{
+		
+		ArrayList<MesoStation> stations = new ArrayList <MesoStation>();
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+    	String stationName = br.readLine();
+    	for(int i = 0; i < 2; ++i){
+    		stationName = br.readLine();
+    	}
+    	stationName = br.readLine();
+    	
+    	//parses string by eliminating front whitespaces and finding the next space
+    	while(stationName != null){
+
+    		stationName = stationName.substring(1,5);
+    		MesoStation tempStation = new MesoStation(stationName);
+    		stations.add(tempStation);
+
+    		stationName = br.readLine();
+    	}
+    	br.close();
+    	
+    	return stations;
+	}
+
+	
+	
+	
 	/**
 	 * Find the average int of the four character station 
 	 * 
@@ -16,7 +54,7 @@ public class MesoInherit extends MesoAbstract {
 	public int[] calAverage() {
 		String id = station.getStID();
 		int total = 0;
-		int [] averages = new int [AVERAGE_ARRAY_SIZE];
+		averages = new int [AVERAGE_ARRAY_SIZE];
 		for(int i=0; i<4; i++){
 			char letter = id.charAt(i);
 			int asciiVal = letter;
@@ -28,14 +66,21 @@ public class MesoInherit extends MesoAbstract {
 		
 		averages[0] = (int) Math.ceil(rawAvg);
 		averages[1] = (int) Math.floor(rawAvg);
-		averages[2] = (int) rawAvg;
+		if((rawAvg % 1) < 0.5){
+			averages[2] = (int) rawAvg;
+		}
+		else{
+			averages[2] = averages[0];
+		}
+		
 		
 		
 		return averages;
 	}
 
 	public String letterAverage() {
-		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
 
