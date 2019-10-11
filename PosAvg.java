5ec9d868c67e4eb4562117cb043ad9d3ef5cf3a1
Stephanie Sheldon <S.Camille.Sheldon>
2019-10-10 20:53:12 -0500

    complete toString method

--- PosAvg.java
+++ PosAvg.java
@@ -13,8 +13,8 @@ public class PosAvg {
 		
 	}
 
-	public String indexOfStation() throws IOException {
-		ArrayList<MesoStation> allStations= MesoInherit.readFile("mesonet.txt");
+	public int indexOfStation() throws IOException {
+		ArrayList<MesoStation> allStations= MesoInherit.readFile("Mesonet.txt");
 		allNames = new ArrayList<String>();
 		for (int i = 0; i < allStations.size(); i++){
 			String name = allStations.get(i).getStID();
@@ -23,23 +23,23 @@ public class PosAvg {
 		
 		trueIndex = allNames.indexOf(stID);
 		int userIndex  = trueIndex + 1;
-	return Integer.toString(userIndex);
+	return userIndex;
 	}
 	
 	public String toString(){
 		//This index is average of NOWA and OILT, NEWP and OKCE, and so on.
-		//Stirng toReturn = "This index is average of"
+		String toReturn = "This index is average of "
+				+ this.getEquivalentAverage(1) + this.getEquivalentAverage(2)
+				+ "and so on.";
+		return toReturn;
 	}
 	
 	public String getEquivalentAverage(int N){
-		try{
+
 			String upperName = allNames.get(trueIndex - N);
 			String lowerName = allNames.get(trueIndex + N);
-			return upperName + " and " + lowerName + ",";
-		}
-		catch(NullPointerException e){
-			return null;
-		}
+			return upperName + " and " + lowerName + ", ";
+		
 		
 	}
 }
