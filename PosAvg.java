86245f7f6f94c9aa60b0264ce38b0febb5588651
Stephanie Sheldon <S.Camille.Sheldon>
2019-10-10 20:00:25 -0500

    complete indexOfStation method

--- PosAvg.java
+++ PosAvg.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class PosAvg {
 
+	private String stID;
+	
+	public PosAvg(String stID) {
+		this.stID = stID;
+		
+	}
+
+	public String indexOfStation() throws IOException {
+		ArrayList<MesoStation> allStations= MesoInherit.readFile("mesonet.txt");
+		ArrayList<String> allNames = new ArrayList<String>();
+		for (int i = 0; i < allStations.size(); i++){
+			String name = allStations.get(i).getStID();
+			allNames.add(name);
+		}
+		
+		int trueIndex = allNames.indexOf(stID);
+		int userIndex  = trueIndex + 1;
+	return Integer.toString(userIndex);
+	}
+	
 }
