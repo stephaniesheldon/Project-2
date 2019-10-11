36351968385a45c277aca8a5f3d5654adfbd5568
Stephanie Sheldon <S.Camille.Sheldon>
2019-10-10 20:24:07 -0500

    complete getEquivalentAverage method

--- PosAvg.java
+++ PosAvg.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class PosAvg {
 
 	private String stID;
+	private ArrayList<String> allNames;
+	private int trueIndex;
 	
 	public PosAvg(String stID) {
 		this.stID = stID;
@@ -13,15 +15,31 @@ public class PosAvg {
 
 	public String indexOfStation() throws IOException {
 		ArrayList<MesoStation> allStations= MesoInherit.readFile("mesonet.txt");
-		ArrayList<String> allNames = new ArrayList<String>();
+		allNames = new ArrayList<String>();
 		for (int i = 0; i < allStations.size(); i++){
 			String name = allStations.get(i).getStID();
 			allNames.add(name);
 		}
 		
-		int trueIndex = allNames.indexOf(stID);
+		trueIndex = allNames.indexOf(stID);
 		int userIndex  = trueIndex + 1;
 	return Integer.toString(userIndex);
 	}
 	
+	public String toString(){
+		//This index is average of NOWA and OILT, NEWP and OKCE, and so on.
+		//Stirng toReturn = "This index is average of"
+	}
+	
+	public String getEquivalentAverage(int N){
+		try{
+			String upperName = allNames.get(trueIndex - N);
+			String lowerName = allNames.get(trueIndex + N);
+			return upperName + " and " + lowerName + ",";
+		}
+		catch(NullPointerException e){
+			return null;
+		}
+		
+	}
 }
